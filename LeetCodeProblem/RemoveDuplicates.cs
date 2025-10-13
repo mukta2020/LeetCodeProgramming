cd6a9a535d7db48c3185eaee7785c799cd0c706b
mukta2020 <me.mukta@yahoo.com>
2025-10-13 14:11:10 -0500

    Roman conversion

---
+++ LeetCodeProblem/RemoveDuplicates.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class RemoveDuplicates
+    {
+
+
+        //public int removeDuplicatesMedium(int[] nums)
+        //{
+        //    int NoDA = 2; //No of duplicates allowed
+        //    if (nums.Length < NoDA) return nums.Length;
+        //    //This is a generic solution
+        //    //if you replace NoDA value to 1, it is solution for "26. Remove Duplicates from Sorted Array"
+        //    //Based on the number of duplicates allowed, It can be replaced with 3, 4, 5, .....
+        //    int P1 = NoDA, P2 = NoDA; //2-pointers, both starting at NoDA
+        //    while (P2 < nums.Length)
+        //    {
+        //        if (nums[P2] != nums[P1 - NoDA])
+        //        {
+        //            nums[P1] = nums[P2];
+        //            P1++;
+        //        }
+        //        P2++;
+        //    }
+        //    return P1;
+        //}
+
+        public static int removeDuplicatesMedium(int[] nums)
+        {
+            int n = 2; //No of duplicates allowed
+            if (nums.Length < n) return nums.Length;
+            int P1 = n;
+            for (int P2 = n; P2 < nums.Length; P2++)
+            {
+                if (nums[P2] != nums[P1 - n])
+                {
+                    nums[P1] = nums[P2];
+                    P1++;
+                }
+            }
+            return P1;
+        }
+        public int RemoveDuplicates1(int[] nums)
+        {
+            int i = 1;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j - 1] != nums[j])
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
+        }
+
+        public static int removeDuplicates1(int[] nums)
+        {
+            int i = 1;  // i for unique,  j  moving  {1,1,2,2,3,3}
+
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[j - 1] != nums[j])
+                {
+                    nums[i] = nums[j]; // unique value assignment
+                    i++;               // unique index increasing
+                }
+            }
+            return i;
+        }
+
+
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int i = 0;   // i for non-removal element count,  j  for full looping on  {3,2,2,3} val = 3
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val) // taking action for non-removal element
+                {
+                    int t = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = t;
+                    i++;
+                }
+            }
+            return i;
+        }
+
+
+    }
+}
---
+++ LeetCodeProblem/RomanToInt.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class RomanToInt
+    {
+
+        public static int RomanToInt1(string s)
+        {
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            d.Add('I', 1); d.Add('V', 5); d.Add('X', 10); d.Add('L', 50); d.Add('C', 100); d.Add('D', 500); d.Add('M', 1000);
+
+            int n = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i + 1 < s.Length && d[s[i]] < d[s[i + 1]])
+                    n -= d[s[i]];
+                else
+                    n += d[s[i]];
+            }
+            return n;
+        }
+
+        public static string IntToRoman(int num)
+        {
+
+            if (num >= 1000) return "M" + IntToRoman(num - 1000);
+
+            if (num >= 900) return "CM" + IntToRoman(num - 900);
+
+            if (num >= 500) return "D" + IntToRoman(num - 500);
+
+            if (num >= 400) return "CD" + IntToRoman(num - 400);
+
+            if (num >= 100) return "C" + IntToRoman(num - 100);
+
+            if (num >= 90) return "XC" + IntToRoman(num - 90);
+
+            if (num >= 50) return "L" + IntToRoman(num - 50);
+
+            if (num >= 40) return "XL" + IntToRoman(num - 40);
+
+            if (num >= 10) return "X" + IntToRoman(num - 10);
+
+            if (num >= 9) return "IX" + IntToRoman(num - 9);
+
+            if (num >= 5) return "V" + IntToRoman(num - 5);
+
+            if (num >= 4) return "IV" + IntToRoman(num - 4);
+
+            if (num >= 1) return "I" + IntToRoman(num - 1);
+
+            return "";
+        }
+    }
+}
