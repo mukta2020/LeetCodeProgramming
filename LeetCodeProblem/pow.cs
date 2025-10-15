337aee67bad8d0c0c0446a215c80e4c58ea8be9b
mukta2020 <me.mukta@yahoo.com>
2025-10-15 01:03:43 -0500

    power

---
+++ LeetCodeProblem/pow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class pow
+    {
+
+        public static int MyPow(int x, int n)  //2 * 2 *2
+        {
+            int r = 1;
+            for (int i = 0; i < n; i++) 
+            {
+                r *= x;            
+            }
+            return r;
+        }
+    }
+}
+
