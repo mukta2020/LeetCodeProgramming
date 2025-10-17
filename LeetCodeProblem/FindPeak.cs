40a541734852b917e89107ce6398887188fc82ad
mukta2020 <me.mukta@yahoo.com>
2025-10-17 14:31:37 -0500

    peak and reverse word

---
+++ LeetCodeProblem/FindPeak.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class FindPeak
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            if (nums.Length == 1)
+                return 0;
+
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+                {
+                    return i;
+                }
+            }
+
+            if (nums[0] > nums[1])
+            {
+                return 0;
+            }
+            else if (nums[nums.Length - 1] > nums[nums.Length - 2])
+            {
+                return nums.Length - 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ LeetCodeProblem/ReverseWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class ReverseWords
+    {
+        static public string ReverseWords1(string s)
+        {
+            s.Trim(' ');
+            string str = "";
+            var words = s.Split(' ');
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                if (words[i] == "")
+                    continue;
+                str += words[i] + " ";
+            }
+            return str.Trim();
+
+        }
+    }
+}
