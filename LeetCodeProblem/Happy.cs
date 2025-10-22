3556a5a63ff98f9bd16932836d86170ce6ac7ad0
mukta2020 <me.mukta@yahoo.com>
2025-10-22 10:06:18 -0500

    happy number

---
+++ LeetCodeProblem/Happy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class Happy
+    {
+        public static bool IsHappy(int n)
+        {
+            int i = 0;
+            while (i < 50 && n != 0)
+            {
+                if (n == 1) { return true; }
+                string s = n.ToString();
+                int sum = 0;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    int d = Convert.ToInt32(s[j].ToString());
+                    sum += d * d;
+                }
+                n = sum;
+                i++;
+            }
+            return false;
+
+        }
+
+    }
+}
---
+++ LeetCodeProblem/MySqrt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class MySqrt
+    {
+        public static int MySqrt1(int x)
+        {
+            int l = 0, h = x;
+            while (l <= h)
+            {
+                int mid = (l + h) / 2;
+                if ((mid == x / mid))
+                    return mid;
+                else if (mid < x / mid)
+                    l = mid + 1;
+                else
+                    h = mid - 1;
+            }
+            return h;
+        }
+        public static int MySqrt2(int x)
+        {
+            int d = x / 2;
+            while (d > 0)
+            {
+                if (d * d == x) return d;
+                else
+                    d = d / 2;
+            }
+            return d;
+
+        }
+
+    }
+}
