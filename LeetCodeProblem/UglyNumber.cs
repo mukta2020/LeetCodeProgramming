76eaae82f74d75dbe8c86c452adc4d8d1814bae0
mukta2020 <me.mukta@yahoo.com>
2025-10-24 15:42:48 -0500

    ugly number

---
+++ LeetCodeProblem/UglyNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class UglyNumber
+    {
+        public static bool IsUgly(int n)
+        {
+
+            while (n > 0)
+            {
+                if (n == 1) return true;
+                if (n % 2 == 0)
+                    n = n / 2;
+                else if (n % 3 == 0)
+                    n = n / 3;
+                else if (n % 5 == 0)
+                    n = n / 5;
+                else
+                    return false;
+            }
+            return false;
+
+        }
+    }
+}
