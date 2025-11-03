292ece3b2e29cb3cd63370d930a8a3dc839cc7ea
mukta2020 <me.mukta@yahoo.com>
2025-11-02 22:53:47 -0600

    add digit problem

---
+++ LeetCodeProblem/AddDigit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class AddDigit
+    {
+        public static int AddDigits(int num)
+        {
+            while (num > 9)
+            {
+                string s = num.ToString();
+                int total = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    int d = Convert.ToInt32(s[i].ToString());
+                    total += d;
+                }
+                num = total;
+            }
+            return num;
+
+        }
+    }
+}
