fbdb1525378e13aa787ac0b99f3576dc27857a10
mukta2020 <me.mukta@yahoo.com>
2025-12-11 23:26:16 -0600

    CheckPerfectNumber

---
+++ LeetCodeProblem/CheckPerfectNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class CheckPerfectNumber
+    {
+        public static bool CheckPerfectNum(int num)
+        {
+            int s = 1;
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    s += num / i;
+                }
+            }
+
+            if (num == s) return true;
+            else return false;
+        }
+
+    }
+}
