a7510f39421c5b1764eb1a4bd9f3d0d68a43ac57
mukta2020 <me.mukta@yahoo.com>
2025-12-25 23:16:40 -0600

    IsToeplitzMatrix

---
+++ LeetCodeProblem/ToeplitzMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class ToeplitzMatrix
+    {
+        public static bool IsToeplitzMatrix(int[][] matrix)
+        {
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            for (int r = 0; r < rows - 1; r++)
+            {
+                for (int c = 0; c < cols - 1; c++)
+                {
+                    if (matrix[r][c] != matrix[r + 1][c + 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool IsToeplitzMatrix2(int[][] matrix)
+        {
+            for (int r = 0; r < matrix.Length; r++)
+                for (int c = 0; c < matrix[0].Length; c++)
+                    if (r > 0 && c > 0 && matrix[r - 1][c - 1] != matrix[r][c])
+                        return false;
+            return true;
+        }
+    }
+}
