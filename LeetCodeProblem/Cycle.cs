5ad09a75c3a9d49c55aa8315b018308b5693d394
mukta2020 <me.mukta@yahoo.com>
2025-12-26 23:52:18 -0600

    HasCycle

---
+++ LeetCodeProblem/Cycle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class Cycle
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            int res = -1;
+            int c = 0;
+            while (head != null)
+            {
+                if (!d.ContainsKey(head.val))
+                {
+                    d.Add(head.val, c);
+                }
+                else
+                {
+                    res = d[head.val] + 1;
+                    break;
+                }
+                c++;
+                head = head.next;
+            }
+            return res > 0;
+
+        }
+
+        public bool hasCycle(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return false; // No cycle if there are less than two nodes.
+            }
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next; // Move one step.
+                fast = fast.next.next; // Move two steps.
+
+                if (slow == fast)
+                {
+                    return true; // Cycle detected.
+                }
+            }
+
+            return false; // No cycle found.
+        }
+
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
