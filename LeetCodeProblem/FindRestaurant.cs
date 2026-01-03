2340dd7292a0cb2d346bd38a07722aa560683c2e
mukta2020 <me.mukta@yahoo.com>
2026-01-03 12:03:48 -0600

    Minimum Index Sum of Two list

---
+++ LeetCodeProblem/FindRestaurant.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem
+{
+    public class FindRestaurants
+    {
+        public static string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            int leastIndexSum = int.MaxValue;
+            List<string> l = new List<string>();
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                string item = list1[i];
+                for (int j = 0; j < list2.Length; j++)
+                {
+                    if (item == list2[j])
+                    {
+                        if ((i + j) == leastIndexSum)
+                        {
+                            l.Add(item);
+                            leastIndexSum = i + j;
+                        }
+                        else if ((i + j) < leastIndexSum)
+                        {
+                            if (l.Count > 0)
+                            {
+                                l.RemoveAt(l.Count - 1);
+                            }
+                            l.Add(item);
+                            leastIndexSum = i + j;
+                        }
+                    }
+
+                }
+
+            }
+            return l.ToArray();
+
+        }
+
+    }
+}
